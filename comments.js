78d2244f091c841b225e21f98e00a21df135fb1e
Kenytt Avery <kavery@fullerton.edu>
2026-06-17 22:13:01 -0700

    Refactored to React

--- comments.js
+++ comments.js
@@ -1,87 +1,186 @@
+import React, { useEffect, useState } from 'https://esm.sh/react@18.3.1?dev'
+import { createRoot } from 'https://esm.sh/react-dom@18.3.1/client?dev'
+
 // https://jsonplaceholder.typicode.com/guide/
 
-async function downloadPosts (page = 1) {
+async function downloadPosts(page = 1) {
   const postsURL = `https://jsonplaceholder.typicode.com/posts?_page=${page}`
   const response = await fetch(postsURL)
   const articles = await response.json()
   return articles
 }
 
-async function downloadComments (postId) {
+async function downloadComments(postId) {
   const commentsURL = `https://jsonplaceholder.typicode.com/posts/${postId}/comments`
   const response = await fetch(commentsURL)
   const comments = await response.json()
   return comments
 }
 
-async function getUserName (userId) {
+async function getUserName(userId) {
   const userURL = `https://jsonplaceholder.typicode.com/users/${userId}`
   const response = await fetch(userURL)
   const user = await response.json()
   return user.name
 }
 
-function getArticleId (comments) {
-  const article = comments.previousElementSibling
-  const data = article.dataset
-  return data.postId
-}
-
-const main = document.querySelector('main')
-
-const posts = await downloadPosts(2)
-for (const post of posts) {
-  const author = await getUserName(post.userId)
-
-  const article = document.createElement('article')
-  const body = post.body.replaceAll('\n', '<br>')
-  article.dataset.postId = post.id
-  article.innerHTML = `
-    <h2>${post.title}</h2>
-    <aside>by <span class="author">${author}</span></aside>
-    <p>
-      ${body}
-    </p>
-  `
-  main.appendChild(article)
-
-  const details = document.createElement('details')
-  details.innerHTML = `
-    <summary>See what our readers had to say...</summary>
-    <section>
-        <header>
-            <h3>Comments</h3>
-        </header>
-    </section>
-  `
-  main.appendChild(details)
-}
-
-const details = document.getElementsByTagName('details')
-for (const detail of details) {
-  detail.addEventListener('toggle', async event => {
-    if (!detail.open) {
-      return
+function renderBody(text) {
+  return text.split('\n').flatMap((line, index) => {
+    if (index === 0) {
+      return line
     }
 
-    const asides = detail.getElementsByTagName('aside')
-    const commentsWereDownloaded = asides.length > 0
-    if (!commentsWereDownloaded) {
-      const articleId = getArticleId(detail)
-      const comments = await downloadComments(articleId)
-      for (const comment of comments) {
-        const body = comment.body.replaceAll('\n', '<br>')
-        const aside = document.createElement('aside')
-        aside.innerHTML = `
-          <p>
-            ${body}
-          <p>
-          <p><small>${comment.name}</small></p>
-        `
-
-        const section = detail.querySelector('section')
-        section.appendChild(aside)
-      }
-    }
+    return [React.createElement('br', { key: `break-${index}` }), line]
   })
 }
+
+function PostCard({ post }) {
+  const [isOpen, setIsOpen] = useState(false)
+  const [comments, setComments] = useState([])
+  const [commentsLoaded, setCommentsLoaded] = useState(false)
+  const [commentsError, setCommentsError] = useState('')
+  const [loadingComments, setLoadingComments] = useState(false)
+
+  useEffect(() => {
+    if (!isOpen || commentsLoaded) {
+      return undefined
+    }
+
+    let cancelled = false
+
+    async function loadComments() {
+      setLoadingComments(true)
+      setCommentsError('')
+
+      try {
+        const downloadedComments = await downloadComments(post.id)
+        if (!cancelled) {
+          setComments(downloadedComments)
+          setCommentsLoaded(true)
+        }
+      } catch {
+        if (!cancelled) {
+          setCommentsError('Unable to load comments.')
+        }
+      } finally {
+        if (!cancelled) {
+          setLoadingComments(false)
+        }
+      }
+    }
+
+    loadComments()
+
+    return () => {
+      cancelled = true
+    }
+  }, [commentsLoaded, isOpen, post.id])
+
+  return React.createElement(
+    React.Fragment,
+    null,
+    React.createElement(
+      'article',
+      { 'data-post-id': post.id },
+      React.createElement('h2', null, post.title),
+      React.createElement(
+        'aside',
+        null,
+        'by ',
+        React.createElement('span', { className: 'author' }, post.author)
+      ),
+      React.createElement('p', null, renderBody(post.body))
+    ),
+    React.createElement(
+      'details',
+      {
+        open: isOpen,
+        onToggle: event => setIsOpen(event.currentTarget.open)
+      },
+      React.createElement('summary', null, 'See what our readers had to say...'),
+      React.createElement(
+        'section',
+        null,
+        React.createElement(
+          'header',
+          null,
+          React.createElement('h3', null, 'Comments')
+        ),
+        loadingComments ? React.createElement('p', null, 'Loading comments...') : null,
+        commentsError ? React.createElement('p', null, commentsError) : null,
+        comments.map(comment =>
+          React.createElement(
+            'aside',
+            { key: comment.id },
+            React.createElement('p', null, renderBody(comment.body)),
+            React.createElement('p', null, React.createElement('small', null, comment.name))
+          )
+        )
+      )
+    )
+  )
+}
+
+function App() {
+  const [posts, setPosts] = useState([])
+  const [loadingPosts, setLoadingPosts] = useState(true)
+  const [postsError, setPostsError] = useState('')
+
+  useEffect(() => {
+    let cancelled = false
+
+    async function loadPosts() {
+      try {
+        const downloadedPosts = await downloadPosts(2)
+        const authorCache = new Map()
+
+        const postsWithAuthors = await Promise.all(
+          downloadedPosts.map(async post => {
+            if (!authorCache.has(post.userId)) {
+              authorCache.set(post.userId, await getUserName(post.userId))
+            }
+
+            return {
+              ...post,
+              author: authorCache.get(post.userId)
+            }
+          })
+        )
+
+        if (!cancelled) {
+          setPosts(postsWithAuthors)
+        }
+      } catch {
+        if (!cancelled) {
+          setPostsError('Unable to load posts.')
+        }
+      } finally {
+        if (!cancelled) {
+          setLoadingPosts(false)
+        }
+      }
+    }
+
+    loadPosts()
+
+    return () => {
+      cancelled = true
+    }
+  }, [])
+
+  if (loadingPosts) {
+    return React.createElement('p', null, 'Loading posts...')
+  }
+
+  if (postsError) {
+    return React.createElement('p', null, postsError)
+  }
+
+  return React.createElement(
+    React.Fragment,
+    null,
+    posts.map(post => React.createElement(PostCard, { key: post.id, post }))
+  )
+}
+
+createRoot(document.querySelector('main')).render(React.createElement(App))
